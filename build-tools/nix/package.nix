32b1e536f1246e7c4842fb7a7cd690fe26787651
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2024-01-24 12:21:09 +0100

    wip

--- build-tools/nix/package.nix
+++ build-tools/nix/package.nix
@@ -8,16 +8,37 @@
 , libopus
 , lib
 , vulkan-loader
-
+, mesa
+, darwin
+, stdenv
+, makeWrapper
+, x264
 }:
 let
+  ffmpeg =
+    (if stdenv.isDarwin then
+      (ffmpeg_6-headless.override {
+        x264 = x264.overrideAttrs (old: {
+          postPatch = old.postPatch + ''
+            substituteInPlace Makefile --replace '$(if $(STRIP), $(STRIP) -x $@)' '$(if $(STRIP), $(STRIP) -S $@)'
+          '';
+        });
+      })
+    else
+      ffmpeg_6-headless
+    );
   buildInputs = [
+    ffmpeg
     openssl
-    pkg-config
-    ffmpeg_6-headless
     libopus
     libGL
+    mesa.drivers
     vulkan-loader
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Metal
+    darwin.apple_sdk.frameworks.Foundation
+    darwin.apple_sdk.frameworks.QuartzCore
+    darwin.libobjc
   ];
   rpath = lib.makeLibraryPath buildInputs;
 in
@@ -26,13 +47,20 @@ rustPlatform.buildRustPackage {
   version = "0.2.0-rc.1";
   src = ../..;
   cargoSha256 = "sha256-KwwfdE6o3drmhzClwoBjwjXLVKYuDgq/SDG5ZGsqzCU=";
+
   buildNoDefaultFeatures = true;
   doCheck = false;
+
   inherit buildInputs;
-  nativeBuildInputs = [ pkg-config llvmPackages_16.clang cmake ];
+  nativeBuildInputs = [ pkg-config llvmPackages_16.clang cmake makeWrapper ];
+
   env.LIBCLANG_PATH = "${llvmPackages_16.libclang.lib}/lib";
+
   postFixup = ''
     patchelf --set-rpath ${rpath} $out/bin/video_compositor
+    wrapProgram $out/bin/video_compositor \
+      --prefix XDG_DATA_DIRS : "${mesa.drivers}/share" \
+      --prefix LD_LIBRARY_PATH : "${mesa.drivers}/lib"
   '';
 }
         
